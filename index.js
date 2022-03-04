f40d6f41169d355f33307358d4e1d0884e6b732e
Lucas Becerra <lucbece@gmail.com>
2022-03-04 02:08:52 -0300

    Cleaning up the mess

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import winston from 'winston';
-import CloudWatchTransport from 'winston-aws-cloudwatch'
 
 var NODE_ENV = process.env.NODE_ENV || 'development'
 
@@ -7,48 +6,19 @@ const logger = winston.createLogger({
     level: process.env.LOG_LEVEL || 'info',
     format: winston.format.json(),
     transports: [
-        new winston.transports.Console({
-            timestamp: true,
-            colorize: true,
-        })
+        new winston.transports.Console()
     ]
 });
 
-var config = {
-    logGroupName: 'test-log-group',
-    logStreamName: NODE_ENV,
-    createLogGroup: true,
-    createLogStream: true,
-    awsConfig: {
-        accessKeyId: process.env.CLOUDWATCH_ACCESS_KEY_ID,
-        secretAccessKey: process.env.CLOUDWATCH_SECRET_ACCESS_KEY,
-        region: process.env.CLOUDWATCH_REGION
-    },
-    formatLog: function (item) {
-        return item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
-    }
-}
-
-if (NODE_ENV != 'development') {
-    logger.add(new CloudWatchTransport(config))
-}
-
-logger.level = process.env.LOG_LEVEL || 'debug'
-logger.stream = {
-    write: function (message, encoding) {
-        logger.info(message)
-    }
-};
-
 export const handler = async (event, context) => {
     logger.defaultMeta = {requestId: context.awsRequestId}
+    logger.silly('This message is not supposed to be seen in CloudWatch')
     logger.log(logger.level, 'Hello World!');
-    logger.error(new Error('This is a test error'));
+    logger.error('This is an error');
     return {
         statusCode: 200,
         body: JSON.stringify({
             message: 'Execution was successful',
-            input: event,
         }),
     }
 };
